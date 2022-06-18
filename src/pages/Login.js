f653b20408b08021083947ee1e91eb53301f4d5e
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-18 12:15:16 +0300

    feat: implementing the login page

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/Form";
+
+function Login({login}){
+    
+    return (
+        <>
+        <Form type = "login"/>
+        </>
+    )
+}
+
+export default Login;
