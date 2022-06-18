0a9b21b165036c89e50d408ed9f4641597a2c037
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-18 12:18:35 +0300

    feat: adding routing to the APP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   
+  return(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/login" element = {
+        <Login />
+      }></Route>
+      <Route path="/register" element = {
+        <Register/>
+      }></Route>
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
